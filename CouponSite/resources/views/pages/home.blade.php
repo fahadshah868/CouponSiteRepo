a16bd04e76acee7f5e153f9a59d48064f0738cb8
fahad shah <fahadshah868>
2018-08-13 19:03:13 +0500

    modifications

--- CouponSite/resources/views/pages/home.blade.php
+++ CouponSite/resources/views/pages/home.blade.php
@@ -17,15 +17,38 @@
   </div>
   
   <div class="body-main-content">
-      <br><br>
-      <div class="today-deals-main-content">
-        <div class="today-deals-heading">
-          <font>Today's Hot Deals</font>
-        </div>
-        <div class="today-deals-products-content">
-            <div class="today-deals-product">
-              <img src="https://i2.wp.com/www.deanesofcheddar.co.uk/wp-content/uploads/2016/06/deane_and_Sons-formal_hire-350x300.jpg" />
-              <div class="today-deals-product-description">
+    <div class="today-deals-main-content">
+      <div class="today-deals-heading">
+        <font>Today's Hot Deals</font>
+      </div>
+      <div class="today-deals-products-content">
+          <div class="today-deals-product">
+            <img src="https://i2.wp.com/www.deanesofcheddar.co.uk/wp-content/uploads/2016/06/deane_and_Sons-formal_hire-350x300.jpg" />
+            <div class="today-deals-product-description">
+              <div class="today-deals-product-description-brandlogo-content">
+                <img class="today-deals-product-description-brandlogo" src="https://www.spellbrand.com/images/blog/images/PacSun-teen-clothing-logo-design.jpg" />
+              </div>
+              <div>
+                <h6>Upto 40% Off</h6>
+                <p>Pacsun Sale</p>
+              </div>
+            </div>
+          </div>
+          <div class="today-deals-product">
+            <img src="http://www.thedorchester.com.au/site/wp-content/uploads/2016/05/family-hols-350x300.jpg" />
+            <div class="today-deals-product-description">
+              <div class="today-deals-product-description-brandlogo-content">
+                  <img class="today-deals-product-description-brandlogo" src="https://www.spellbrand.com/images/blog/images/PacSun-teen-clothing-logo-design.jpg" />
+                </div>
+                <div>
+                  <h6>Upto 40% Off</h6>
+                  <p>Pacsun Sale</p>
+                </div>
+              </div>
+          </div>
+          <div class="today-deals-product">
+            <img style="cursor:pointer;" src="https://www.centrasota.com/CTRA/media/Images/design/Country-Store-350-x-300.jpg?ext=.jpg" />
+            <div class="today-deals-product-description">
                 <div class="today-deals-product-description-brandlogo-content">
                   <img class="today-deals-product-description-brandlogo" src="https://www.spellbrand.com/images/blog/images/PacSun-teen-clothing-logo-design.jpg" />
                 </div>
@@ -34,48 +57,24 @@
                   <p>Pacsun Sale</p>
                 </div>
               </div>
-            </div>
-            <div class="today-deals-product">
-              <img src="http://www.thedorchester.com.au/site/wp-content/uploads/2016/05/family-hols-350x300.jpg" />
-              <div class="today-deals-product-description">
-                <div class="today-deals-product-description-brandlogo-content">
-                    <img class="today-deals-product-description-brandlogo" src="https://www.spellbrand.com/images/blog/images/PacSun-teen-clothing-logo-design.jpg" />
-                  </div>
-                  <div>
-                    <h6>Upto 40% Off</h6>
-                    <p>Pacsun Sale</p>
-                  </div>
-                </div>
-            </div>
-            <div class="today-deals-product">
-              <img style="cursor:pointer;" src="https://www.centrasota.com/CTRA/media/Images/design/Country-Store-350-x-300.jpg?ext=.jpg" />
-              <div class="today-deals-product-description">
-                  <div class="today-deals-product-description-brandlogo-content">
-                    <img class="today-deals-product-description-brandlogo" src="https://www.spellbrand.com/images/blog/images/PacSun-teen-clothing-logo-design.jpg" />
-                  </div>
-                  <div>
-                    <h6>Upto 40% Off</h6>
-                    <p>Pacsun Sale</p>
-                  </div>
-                </div>
-            </div>
-        </div>
+          </div>
       </div>
-      <div class="top-offers-main-content">
-        <div class="top-offers-heading">
-          <font><b>Top Offers</b></font>
-        </div>
-        <div class="top-offers-products">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+    </div>
+    <div class="top-offers-main-content">
+      <div class="top-offers-heading">
+        <font><b>Top Offers</b></font>
       </div>
+      <div class="top-offers-products">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   </div>
   <script>
     $(document).ready(function() {  
