f49f0eff0ca6bdde399bc006424060d0a9248f85
fahad shah <fahadshah868>
2018-10-27 20:50:19 +0500

    additions

--- CouponSite/resources/views/pages/store/storecoupons.blade.php
+++ CouponSite/resources/views/pages/store/storecoupons.blade.php
@@ -4,35 +4,37 @@
 
 @section('content')
 
-<div style="width: 1150px; margin: 30px auto; display: flex; flex-direction: row;">
-    <div class="side-bar" style="width: 250px; padding: 0 20px;">
-        <a href="#" style="margin-bottom: 30px; color: black; text-decoration: none;">
-            <div style="border: 1px solid grey; width: 100%; padding: 20px; background-color: #fff; display: flex; align-items: center; justify-content:center; flex-direction: column; text-align:center;">
-                <div style="width: 150px; height: 150px; margin-bottom: 10px;">
+<div class="store-coupons-main-container">
+    <div class="store-coupons-sidebar">
+        <div class="store-coupons-sidebar-storelogo-container">
+            <a class="store-coupons-sidebar-store-link" href="#">
+                <div class="store-coupons-sidebar-storelogo">
                     <img src="https://igx.4sqi.net/img/general/200x200/38757329_V6X_cPjnJ2QsS2w-P7Ret6Lfm8T7J-i4dMRtGBbf-B4.jpg" style="width: 100%; height: 100%;"/>
                 </div>
-                <div>Kohl's</div>
-            </div>
-        </a>
-        <hr>
-        <div style="width: 100%; text-align:center; font-weight: bold; font-size: 24px; font-family: calibri;">50 Offers Available</div>
-        <hr>
+                <div class="store-coupons-sidebar-storetitle">Kohl's</div>
+            </a>
+        </div>
+        <hr style="border-top: 1px solid #d1d1d1; width: 100%;">
+        <div class="store-coupons-sidebar-offers-availability">50 Offers Available</div>
+        <hr style="border-top: 1px solid #d1d1d1; width: 100%;">
     </div>
-    <div class="detail-bar" style="width: 900px; padding: 0 20px;">
-        <div class="heading" style="width: 100%; margin-bottom: 20px; font-size: 30px; font-weight: bold;">Kohl's Coupons</div>
-        <div class="coupons-list" style="width: 100%; display: flex; flex-direction: column;">
+    <div class="store-coupons-detailbar">
+        <div class="store-coupons-detailbar-heading">Kohl's Coupons</div>
+        <div class="store-coupons-detailbar-coupons-list-container">
             @for($i=1; $i<= 10; $i++)
-            <div class="coupon-container" style="width:100%; margin-bottom: 15px;">
-                <a href="http://www.google.com" style="color: black; text-decoration: none; border: 1px solid grey; width: 100%; padding: 20px; background-color: #fff; display: flex; flex-direction: row; flex-wrap:nowrap;">
-                    <div style="width: 100px; height: 100px;">
-                        <img src="https://igx.4sqi.net/img/general/200x200/38757329_V6X_cPjnJ2QsS2w-P7Ret6Lfm8T7J-i4dMRtGBbf-B4.jpg" style="width: 100%; height: 100%; border: 1px solid black;"/>
+            <div class="store-coupons-detailbar-coupon-container">
+                <a href="#" class="store-coupons-detailbar-coupon-link">
+                    <div class="store-coupons-detailbar-coupon-title">
+                        <span style="font-size: 25px; font-weight: bold;">40%</span>
+                        <span style="font-size: 18px;">Off</span>
                     </div>
-                    <div style="width: calc(100% - 320px); margin: 0 10px; padding: 5px; display: flex; flex-direction: column;">
-                        <div style="">Upto 40% off total purchase with discout</div>
-                        <div style="">Code</div>
+                    <div class="store-coupons-detailbar-offer-container">
+                        <div class="store-coupons-detailbar-offertitle">40% off total purchase with discout</div>
+                        <div class="store-coupons-detailbar-offerlabel">Code</div>
+                        <div class="store-coupons-detailbar-coupon-expires">Expires: 30/10/18</div>
                     </div>
-                    <div style="width: 200px; display: flex; flex-direction: column; justify-content:center;">
-                        <button style="width: 100%; height: 40px; cursor:pointer;">View Code</button>
+                    <div class="store-coupons-detailbar-offer-button-container">
+                        <button id="offer-button">View Code</button>
                     </div>
                 </a>
             </div>
