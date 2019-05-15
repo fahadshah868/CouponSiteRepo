fb825c28825279f0b9ba9f046c9271ded2b07797
fahad shah <fahadshah868>
2019-05-15 22:44:51 +0500

    updations

--- CouponSite/resources/views/partialviews/socialshare.blade.php
+++ CouponSite/resources/views/partialviews/socialshare.blade.php
@@ -1,23 +1,20 @@
 <div class="social-buttons">
-    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}"
-        target="_blank">
+    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}" target="_blank">
         <div class="social-icon-container">
             <img src="{{asset('images/social-icons/facebook.png')}}">
         </div>
     </a>
-    <a href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}"
-        target="_blank">
+    <a href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}" target="_blank">
         <div class="social-icon-container">
             <img src="{{asset('images/social-icons/twitter.png')}}">
         </div>
     </a>
-    <a href="https://plus.google.com/share?url={{ urlencode($url) }}"
-        target="_blank">
+    <a href="https://plus.google.com/share?url={{ urlencode($url) }}" target="_blank">
         <div class="social-icon-container">
             <img src="{{asset('images/social-icons/googleplus.png')}}">
         </div>
     </a>
-    <a href="https://pinterest.com/pin/create/button/?{{ 
+    <a href="https://pinterest.com/pin/create/button/?{{
         http_build_query([
             'url' => $url,
             'media' => $image,
@@ -28,8 +25,7 @@
                 <img src="{{asset('images/social-icons/pinterest.png')}}">
         </div>
     </a>
-    <a href="https://wa.me/?text={{ urlencode($url) }}"
-        target="_blank">
+    <a href="https://wa.me/?text={{ urlencode($url) }}" target="_blank">
         <div class="social-icon-container">
             <img src="{{asset('images/social-icons/whatsapp.png')}}">
         </div>
